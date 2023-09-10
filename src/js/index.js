49f42e8db8cc20b9bc94a36164fda7505864d434
Anton Zvonarev <124657315+404iy@users.noreply.github.com>
2023-09-10 19:07:39 +0300

    Main

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '@popperjs/core';
+import 'bootstrap';
+import './ssm';
+
+const body = document.getElementById('body');
+if (body) {
+    let today = new Date();// Вывод даты
+    let year = today.getFullYear();
+    let cYear = document.querySelectorAll('.cYear');
+    cYear.forEach(i => i.innerHTML = year.toString());
+}
