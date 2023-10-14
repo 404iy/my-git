2a17c4056ce61d065f5651d67f3eca17cc7c270a
Anton Zvonarev <124657315+404iy@users.noreply.github.com>
2023-10-14 16:45:51 +0300

    Main

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,14 @@ if (body) {
     let year = today.getFullYear();
     let cYear = document.querySelectorAll('.cYear');
     cYear.forEach((i) => i.innerHTML = year.toString());
+
+    const header = document.querySelector('.header__nav');
+
+    window.addEventListener('scroll', () => {
+        const scrollPos = window.scrollY;
+        const maxScroll = 700;
+        let opacity = scrollPos / maxScroll;
+        opacity = Math.min(0.55, opacity);
+        header.style.backgroundColor = `rgba(0, 0, 0, ${opacity.toFixed(2)})`;
+    });
 }
