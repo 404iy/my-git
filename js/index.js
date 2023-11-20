911cb286a1b27ecb559734aabc2cfb39f8719c92
Anton Zvonarev <124657315+404iy@users.noreply.github.com>
2023-11-20 23:05:23 +0300

    Main

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let modal = document.getElementById("myModal");
 
 let closeModalBtn = document.getElementById("closeModalBtn");
-
+let openModalBtn = document.getElementById('openModalBtn');
 
 openModalBtn.addEventListener("click", function () {
     modal.style.display = "flex";
