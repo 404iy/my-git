1283524b21382f46817527130310718f32dc73fc
Anton Zvonarev <124657315+404iy@users.noreply.github.com>
2023-11-27 23:23:19 +0300

    Main

--- src/js/hbshelpers/index.ts
+++ src/js/hbshelpers/index.ts
@@ -1,5 +1,6 @@
 import Handlebars = require('handlebars/runtime');
 
+
 Handlebars.registerHelper('not_null', (value: any, options: any) => {
     if (value !== null && value !== undefined) {
         return options.fn(value);
